3fb55ca885991b636cbc2bb373fcdc855555e131
Marc Partensky <marc.partensky@gmail.com>
2020-11-02 18:13:46 +0100

    working window

--- Pong-Tutorial/pong/src/main.rs
+++ Pong-Tutorial/pong/src/main.rs
@@ -1,10 +1,14 @@
-#!/usr/bin/env run-cargo-script
+#!/usr/bin/env cargo run
 
 use tetra::graphics::{self, Color};
 use tetra::{Context, ContextBuilder, State};
 
-fn main() {
-    println!("test");
+struct GameState {}
+impl State for GameState {}
+
+fn main() -> tetra::Result {
+    ContextBuilder::new("Pong", 640, 480)
+        .quit_on_escape(true)
+        .build()?
+        .run(|_ctx| Ok(GameState {}))
 }
-
-
