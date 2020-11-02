812aa536a2ee23606c428489cd1b66feccb6a5e3
Marc Partensky <marc.partensky@gmail.com>
2020-11-02 18:16:14 +0100

    gamestate for coloring in blue

--- Pong-Tutorial/pong/src/main.rs
+++ Pong-Tutorial/pong/src/main.rs
@@ -3,12 +3,21 @@
 use tetra::graphics::{self, Color};
 use tetra::{Context, ContextBuilder, State};
 
-struct GameState {}
-impl State for GameState {}
-
 fn main() -> tetra::Result {
     ContextBuilder::new("Pong", 640, 480)
         .quit_on_escape(true)
         .build()?
         .run(|_ctx| Ok(GameState {}))
 }
+
+struct GameState {}
+
+impl State for GameState {
+    fn draw(&mut self, ctx: &mut Context) -> tetra::Result {
+        // Feel free to change the color to something of your choice!
+        graphics::clear(ctx, Color::rgb(0.392, 0.584, 0.929));
+
+        Ok(())
+    }
+}
+
