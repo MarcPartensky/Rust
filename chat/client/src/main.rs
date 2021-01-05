5e311ccdb098ad7840d5c280f50ba68c5e194136
marc <marc.partensky@gmail.com>
2021-01-05 21:56:37 +0100

    chat app

---
+++ chat/client/src/main.rs
@@ -0,0 +1,42 @@
+use std::io::{self, ErrorKind, REad, Write};
+use std::net::TcpStream;
+use std::sync::mpsc::{self, TryRecvError};
+use std::thread;
+use std::time::Duration;
+
+const LOCAL: &str = "127.0.0.1:6000";
+const MSG_SIZE: usize = 32;
+
+fn main() {
+    let mut clent = TcpStream::connect(LOCAL).expect("stream failed to connect");
+    client.set_nonblocking(true).expect("faield to initiate non-blocking");
+
+    let (tx, rx) = mpsc::channel::<String>();
+
+    thread::spawn(move || loop {
+        let mut buff = vec![0; MSG_SIZE];
+        match client.read_exact(&mut buff) {
+            Ok(_) => {
+                let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                println!("message recv {:?}", msg);
+            },
+            Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+            Err(_) => {
+                println!("connection with server was severed");
+                break;
+            }
+        }
+
+        thread::sleep(Duration::from_millis(100));
+    });
+
+    println!("Write a message:");
+    loop {
+        let mut buff = String::new();
+        io::stdin().read_line(&mut buff).expect("reading from stdin failed");
+        let msg = buff.trim().to_string();
+        if msg == ":quit" || tx.send(msg).is_err() {break}
+    }
+
+    println!("bye bye");
+}
